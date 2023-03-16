b8e2e39475b74ed30879daa182e5440d371fd5a9
sk409 <deepstrap353@gmail.com>
2023-03-16 23:37:50 +0900

    Initial Commit

---
+++ WYID/WYIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  WYIDApp.swift
+//  WYID
+//
+//  Created by 小林聖人 on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct WYIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
